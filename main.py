a109466d892b3101c644dc4a6bdb7690cd8335ce
Keith Maxwell <keith.maxwell@gmail.com>
2019-10-30 20:30:40 +0000

    Refactor

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
+DURATIONS = {"m": 60 * 1000, "h": 60 * 60 * 1000}
+
+
 def main(_input):
     """Return the number of milliseconds in string
 
@@ -5,16 +8,22 @@ def main(_input):
     w week
     d day
     m minute
+
+    In addtion:
+
+    h hour
     """
-    code = "m"
-    milliseconds = 60 * 1000
-    if _input.endswith(code):
-        return float(_input.rstrip(code)) * milliseconds
-    else:
-        return 0
+    result = 0
+
+    for code in DURATIONS:
+        if _input.endswith(code):
+            result += float(_input.rstrip(code)) * DURATIONS[code]
+
+    return result
 
 
 def test_main():
     assert main("") == 0
     assert main("1m") == 60 * 1000
     assert main("1.5m") == 90 * 1000
+    assert main("1h") == 60 * 60 * 1000
